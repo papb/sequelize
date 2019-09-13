b85f7113ae2a25bf7fde805189ac7c72b0095f58
papb <papb1996@gmail.com>
2019-09-12 23:06:30 -0300

    fix

--- src/sscce.js
+++ src/sscce.js
@@ -4,8 +4,6 @@ const { Sequelize, Op, Model, DataTypes } = require('sequelize');
 
 module.exports = async function({ createSequelizeInstance }) {
     const sequelize = createSequelizeInstance();
-
     await sequelize.authenticate();
-
     console.log('Authenticated!');
 };
