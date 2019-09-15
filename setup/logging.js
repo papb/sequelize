3c16c258c0782211a96f8de96ca26fac1019787e
papb <papb1996@gmail.com>
2019-09-14 22:45:33 -0300

    fix

--- setup/logging.js
+++ setup/logging.js
@@ -4,25 +4,18 @@ const _ = require('lodash');
 const chalk = require('chalk');
 
 function isOptionsObject(arg) {
-    console.log('Checking if isOptionsObject:', arg);
-    const result = arg && _.isPlainObject(arg) && Object.prototype.hasOwnProperty.call(arg, '__isOptionsObject__');
-    return result;
+    return arg && _.isPlainObject(arg) && Object.prototype.hasOwnProperty.call(arg, 'logging');
 }
 
 module.exports = function(...args) {
     args = args.filter(x => x !== undefined);
     if (args.length === 0) return;
-
-    const last = args[args.length - 1];
-    const options = isOptionsObject(last) ? args.pop() : {};
-
-    args = args.map((arg, index) => {
-        // If benchmarking option is enabled, the last argument is the elapsed time
-        if (index === args.length - 1 && options.benchmark) {
-            return `[Elapsed time: ${arg} ms]`;
-        }
-        return arg;
-    });
-
-    console.log('[Sequelize]', ...args);
+    if (isOptionsObject(args[args.length - 1])) args.pop();
+    args[0] = chalk.gray(args[0]);
+    if (args.length === 2) {
+        // If benchmarking option is enabled, the logging will have a
+        // second argument last argument which is the elapsed time
+        args[1] = chalk.blue(`[Elapsed time: ${args[1]} ms]`);
+    }
+    console.log('[Sequelize]', ...args, '\n');
 };
